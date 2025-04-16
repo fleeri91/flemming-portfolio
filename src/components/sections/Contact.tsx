a55b3343f9c8c9a50e75d6ca7d3b090e02391d63
Flemming Eriksson <fleeri91@gmail.com>
2025-04-16 16:19:08 +0200

    mobile fix

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -8,8 +8,8 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-sky-500 bg-clip-text text-transparent text-center">
+        <div className="max-w-xl mx-auto px-4">
+          <h2 className="text-2xl sm:text-3xl font-bold mb-16 bg-gradient-to-r from-blue-500 to-sky-500 bg-clip-text text-transparent text-center">
             {
               "Have a question or want to work together? Leave your details and I'll get back to you as soon as possible."
             }
