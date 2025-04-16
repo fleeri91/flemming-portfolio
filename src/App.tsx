e1a0ebc06e49b55a22c58d2f89e2f0e9f611626d
Flemming Eriksson <fleeri91@gmail.com>
2025-04-16 16:08:05 +0200

    scroll block when loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { LoadingScreen } from './components/LoadingScreen'
 import { Home } from './components/sections/Home'
 import { About } from './components/sections/About'
 import { Projects } from './components/sections/Projects'
 import { Contact } from '@components/sections/Contact'
 
+import useScrollBlock from '@hooks/scrollBlock'
+
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
+  const [blockScroll, allowScroll] = useScrollBlock()
+
+  useEffect(() => {
+    if (!isLoaded) {
+      blockScroll()
+    } else {
+      allowScroll()
+    }
+  }, [isLoaded, blockScroll, allowScroll])
 
   return (
     <>
